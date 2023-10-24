0590db3817afd62ad06ed2aa16d09805a8570682
Bankole214 <adegunlenurudeen214@gmail.com>
2023-10-24 15:22:42 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-
-// import the module
 const fs = require('fs');
-
-// The first argument is the file path
-const file = process.argv[2];
-
-// read the file
-fs.readFile(file, 'utf-8', (error, data) => {
-  if (error) {
-    console.error('Error reading file:', error);
-    return;
-  }
-  console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
